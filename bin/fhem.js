ad8c2b2f2e464723c2a1d0564f6924766a042c4b
winne27 <werner@schaeffer.net>
2016-04-20 12:30:53 +0200

    Version 2.2.2

--- bin/fhem.js
+++ bin/fhem.js
@@ -28,7 +28,7 @@ while getopts "p:l:e:n:" opt; do
       ;;
   esac
 done
-
+  
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
