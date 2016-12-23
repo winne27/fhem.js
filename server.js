4a069c79fc45fc6b8f5f9b90d792df68850bcc26
winne27 <werner@schaeffer.net>
2016-12-23 15:49:52 +0100

    Version 2.4.8

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 /*
- ** use this script with socket-auth Version 0.0.4 and newer
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+
  */
 var url = require('url');
 var fs = require('fs');
