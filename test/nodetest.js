176230881ffd3fe82e19138684e59b0d74a3c23c
winne27 <werner@schaeffer.net>
2018-01-23 21:58:07 +0100

    Version 3.0.0

---
+++ test/nodetest.js
@@ -0,0 +1,48 @@
+var changedValues = '';
+var socket = io.connect('ws://your.domain.tld:8086', {
+	timeout: 5000,
+	'sync disconnect on unload' : true
+});
+
+socket.on('connect', function() {
+	socket.emit('getAllValuesOnChange');
+	socket.emit('getAllDevicesOnChange');
+
+	socket.emit('getReadingOnChange', {
+		unit: 'unit-name',
+		reading: 'reading-name'
+	});
+
+	socket.emit('getAllValues', function(data) {
+		var out = '';
+		for (unit in data) {
+			var value = data[unit];
+			out = out + unit + ': ' + value + "<br>";
+		}
+		document.getElementById("container1").innerHTML = out;
+	});
+
+	socket.emit('JsonList2', '', function(data) {
+		console.log(data);
+	});
+});
+
+socket.on('value',function(data) {
+	for (unit in data) {
+		var value = data[unit];
+		changedValues = changedValues + unit + ': ' + value + "<br>";
+	}
+	document.getElementById("container2").innerHTML = changedValues;
+});
+
+socket.on('device',function(data) {
+	console.log(data);
+});
+
+socket.on('reading',function(data) {
+	console.log(data);
+});
+
+socket.on('disconnect', function() {
+	console.log('disconnected from server');
+});
